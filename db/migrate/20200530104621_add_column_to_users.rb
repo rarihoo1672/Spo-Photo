de380d7a3346eb146520ffa2767efa0aace3dc18
Ayato Seto <seto_rarihoo1672@icloud.com>
2020-05-30 20:27:10 +0900

    Add column users table

--- db/migrate/20200530104621_add_column_to_users.rb
+++ db/migrate/20200530104621_add_column_to_users.rb
@@ -2,6 +2,6 @@ class AddColumnToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :nickname, :string, default: ""
     add_column :users, :avatar, :string
-    add_column :users, :introduction, :text, default: ""
+    add_column :users, :introduction, :string, default: ""
   end
 end
