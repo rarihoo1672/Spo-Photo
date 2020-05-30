bd41fb7cc21213544e882db2974e46c07d474c45
Ayato Seto <seto_rarihoo1672@icloud.com>
2020-05-30 19:53:13 +0900

    Fix users maigration file
    生成したファイルの、カラムの型の修正とオプションの追加

--- db/migrate/20200530104621_add_column_to_users.rb
+++ db/migrate/20200530104621_add_column_to_users.rb
@@ -1,7 +1,7 @@
 class AddColumnToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :nickname, :string
+    add_column :users, :nickname, :string, default: ""
     add_column :users, :avatar, :string
-    add_column :users, :introduction, :string
+    add_column :users, :introduction, :text, default: ""
   end
 end
