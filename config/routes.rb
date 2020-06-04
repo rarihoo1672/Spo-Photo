887f3726082c713952fa9bf06efe0672026ad4b0
Ayato Seto <seto_rarihoo1672@icloud.com>
2020-06-05 01:09:34 +0900

    Add routing edit and update of users

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,5 @@ Rails.application.routes.draw do
     get "sign_out", :to => "users/sessions#destroy" 
   end
 
-  resources :users, only: [:show]
-
+  resources :users, only: [:show, :edit, :update]
 end
