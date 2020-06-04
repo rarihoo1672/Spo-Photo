37d771a1fc92a56debcf009487c14790c41f20e0
Ayato Seto <seto_rarihoo1672@icloud.com>
2020-06-05 01:10:08 +0900

    Add update action in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,18 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+  def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+      redirect_to user_path
+    else
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:avatar, :nickname, :introduction)
+  end
 end
