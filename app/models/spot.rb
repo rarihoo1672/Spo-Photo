bc22aa82cc1a6912814d1f1c6acc64dbe5696a5e
Ayato Seto <seto_rarihoo1672@icloud.com>
2020-06-05 04:44:27 +0900

    Add mount_uploader to main_visual column

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -3,4 +3,6 @@ class Spot < ApplicationRecord
 
   validates :name, presence: true, length: {maximum: 25}
   validates :main_visual, presence: true
+
+  mount_uploader :main_visual, MainVisualUploader
 end
