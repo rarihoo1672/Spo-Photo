01467e75d8abac84eb2c4bdcbed9526299de32cd
Ayato Seto <seto_rarihoo1672@icloud.com>
2020-06-05 04:19:40 +0900

    Create spot model

--- db/migrate/20200604183538_create_spots.rb
+++ db/migrate/20200604183538_create_spots.rb
@@ -1,7 +1,13 @@
 class CreateSpots < ActiveRecord::Migration[5.2]
   def change
     create_table :spots do |t|
-
+      t.string :name, null: false, limit: 25
+      t.string :main_visual, null: false
+      t.string :infomation
+      t.string :address
+      t.float :latitude
+      t.float :longitude
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_30_104621) do
+ActiveRecord::Schema.define(version: 2020_06_04_183538) do
+
+  create_table "spots", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", limit: 25, null: false
+    t.string "main_visual", null: false
+    t.string "infomation"
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_spots_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +40,5 @@ ActiveRecord::Schema.define(version: 2020_05_30_104621) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "spots", "users"
 end
