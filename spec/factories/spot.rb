8c43b2615b58c5802163ce062cfdf8da4b2c36b5
Ayato Seto <seto_rarihoo1672@icloud.com>
2020-06-05 22:08:37 +0900

    Fix typo infomation to information in factorybot file

--- spec/factories/spot.rb
+++ spec/factories/spot.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :spot do
     name { "サンプルスポット" }
     main_visual {  Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/test.jpg'), 'image/jpg') }
-    infomation { "サンプルスポット、いい場所です" }
+    information { "サンプルスポット、いい場所です" }
     address { "hoge県huga市hunga" }
   end
 end
