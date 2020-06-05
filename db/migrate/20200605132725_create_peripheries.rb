3b47439fc4ab5839e1f3941cfd95ad4f5e198c33
Ayato Seto <seto_rarihoo1672@icloud.com>
2020-06-05 22:36:08 +0900

    Create periphery model

--- db/migrate/20200605132725_create_peripheries.rb
+++ db/migrate/20200605132725_create_peripheries.rb
@@ -1,7 +1,8 @@
 class CreatePeripheries < ActiveRecord::Migration[5.2]
   def change
     create_table :peripheries do |t|
-
+      t.string :sub_visual
+      t.references :spot, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_05_130125) do
+ActiveRecord::Schema.define(version: 2020_06_05_132725) do
+
+  create_table "peripheries", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "sub_visual"
+    t.bigint "spot_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["spot_id"], name: "index_peripheries_on_spot_id"
+  end
 
   create_table "spots", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", limit: 25, null: false
@@ -40,5 +48,6 @@ ActiveRecord::Schema.define(version: 2020_06_05_130125) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "peripheries", "spots"
   add_foreign_key "spots", "users"
 end
