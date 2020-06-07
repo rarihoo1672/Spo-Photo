c5921002fb4af40618d4a945565b235dce43ba74
Ayato Seto <seto_rarihoo1672@icloud.com>
2020-06-08 02:20:47 +0900

    Fix easier to see in spot.rb
    バラバラに並んでいたため、グループごとに並べ替えた

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,11 +1,10 @@
 class Spot < ApplicationRecord
   belongs_to :user
-
-  validates :name, presence: true, length: {maximum: 25}
-  validates :main_visual, presence: true
-
   has_many :peripheries
 
+  validates :name, presence: true, length: {maximum: 25}
+  
+  validates :main_visual, presence: true
   mount_uploader :main_visual, MainVisualUploader
 
   geocoded_by :address
