87a255986be6ceeba61576590bc315accb2eed77
Ayato Seto <seto_rarihoo1672@icloud.com>
2020-06-07 15:59:40 +0900

    Change time zone of toyko
    タイムゾーンをUTCからJSTに変更

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module SpoPhoto
     config.load_defaults 5.2
     config.i18n.default_locale = :ja
 
+    config.time_zone = 'Asia/Tokyo'
+
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
