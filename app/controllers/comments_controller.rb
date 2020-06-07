0c6b3db55a0324cb1bad16eae53763325605e8fa
Ayato Seto <seto_rarihoo1672@icloud.com>
2020-06-08 08:13:00 +0900

    Add comment function in comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,17 @@
 class CommentsController < ApplicationController
+  def create
+    spot = Spot.find(params[:spot_id])
+    @comment = spot.comments.build(comment_params)
+    if @comment.save
+      redirect_to spot_path(spot)
+    else
+      render :new
+    end
+  end
+  
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, spot_id: params[:spot_id])
+  end
 end
