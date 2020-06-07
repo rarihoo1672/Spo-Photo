52de737cd3c02504ba47a95e27419d6963375b38
Ayato Seto <seto_rarihoo1672@icloud.com>
2020-06-08 05:56:33 +0900

    Create comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_05_132725) do
+ActiveRecord::Schema.define(version: 2020_06_07_205050) do
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "cotent", limit: 30, null: false
+    t.bigint "user_id"
+    t.bigint "spot_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["spot_id"], name: "index_comments_on_spot_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "peripheries", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "sub_visual"
@@ -48,6 +58,8 @@ ActiveRecord::Schema.define(version: 2020_06_05_132725) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "spots"
+  add_foreign_key "comments", "users"
   add_foreign_key "peripheries", "spots"
   add_foreign_key "spots", "users"
 end
