d4729c2013c1395d9a9c8aacf4870846332e0bc0
Ayato Seto <seto_rarihoo1672@icloud.com>
2020-06-08 22:04:36 +0900

    Add routing of likes

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,6 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :edit, :update]
   resources :spots do
     resources :comments, only: [:create, :destroy]
+    resources :likes, only: [:create, :destroy]
   end
 end
