1b44539b9e1558a908158e2e697d6b28382722ff
Ayato Seto <seto_rarihoo1672@icloud.com>
2020-06-08 22:07:51 +0900

    Add create and destroy action in likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,17 @@
 class LikesController < ApplicationController
+  before_action :set_tweet
+
+  def create
+    @like = Like.create(user_id: current_user.id, spot_id: @spot.id)
+  end
+
+  def destroy
+    @like = Like.find_by(user_id: current_user.id, spot_id: @spot.id)
+    @like.destroy
+  end
+
+  private
+    def set_tweet
+      @spot = Spot.find(params[:spot_id])
+    end
 end
