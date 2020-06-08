0c323c95cb59184771664c30a17cea4183b899e6
Ayato Seto <seto_rarihoo1672@icloud.com>
2020-06-08 23:13:49 +0900

    Add likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,17 +1,12 @@
 class LikesController < ApplicationController
-  before_action :set_tweet
-
   def create
-    @like = Like.create(user_id: current_user.id, spot_id: @spot.id)
+    @like = current_user.likes.create(spot_id: params[:spot_id])
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    @like = Like.find_by(user_id: current_user.id, spot_id: @spot.id)
+    @like = Like.find_by(spot_id: params[:spot_id], user_id: current_user.id)
     @like.destroy
+    redirect_back(fallback_location: root_path)
   end
-
-  private
-    def set_tweet
-      @spot = Spot.find(params[:spot_id])
-    end
 end
