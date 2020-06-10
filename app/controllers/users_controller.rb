b4eb7e4ea360842fd40a3f46127c82d37931c178
Ayato Seto <seto_rarihoo1672@icloud.com>
2020-06-10 23:51:52 +0900

    a

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,14 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  
   def show
-    @user = User.find(params[:id])
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    user = User.find(params[:id])
-    if user.update(user_params)
+    if @user.update(user_params)
       redirect_to user_path
       flash[:notice] = "プロフィールを編集しました"
     else
@@ -20,5 +19,9 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:avatar, :nickname, :introduction)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
