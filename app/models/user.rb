5cfcf50359f09cf2691729a0922c0256c849ec52
Ayato Seto <seto_rarihoo1672@icloud.com>
2020-06-10 21:47:40 +0900

    Improve sort easy to see in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,20 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise  :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :validatable
 
-  mount_uploader :avatar, AvatarUploader
+  has_many        :spots
+  has_many        :comments,     dependent: :destroy
+  has_many        :likes,        dependent: :destroy
+  has_many        :liked_spotss, through: :likes, source: :spot
 
-  validates :nickname,  presence: true, uniqueness: true,
-                        length: {maximum: 12}
+  validates       :nickname,     presence: true,  uniqueness: true, 
+                  length: { minimum: 1, maximum: 12 }
+  validates       :introduction, length: { muximum: 1000 }
+  
+  mount_uploader  :avatar, AvatarUploader
 
-  has_many :spots
-  has_many :comments, dependent: :destroy
-  has_many :likes, dependent: :destroy
-  has_many :liked_spotss, through: :likes, source: :spot
   def already_liked?(spot)
     self.likes.exists?(spot_id: spot.id)
   end
