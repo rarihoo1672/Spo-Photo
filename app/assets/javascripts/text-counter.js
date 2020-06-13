3eabe100cffcd7aaf46e0617afb9a313fcaa5af3
Ayato Seto <seto_rarihoo1672@icloud.com>
2020-06-14 00:05:16 +0900

    Create text counter function js

---
+++ app/assets/javascripts/text-counter.js
@@ -0,0 +1,8 @@
+$(document).on('turbolinks:load', ()=> {
+  $(function(){
+    $("#js-counter").keyup(function(){
+      let count = $(this).val().length;
+      $("#show-count").text(count);
+    });
+  });
+});
