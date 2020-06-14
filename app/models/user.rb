4bf21561c4b04beaf1b665f53f91b61114309c01
Ayato Seto <seto_rarihoo1672@icloud.com>
2020-06-15 04:03:59 +0900

    Improve easy dispay in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise  :database_authenticatable, :registerable,
           :recoverable, :rememberable, :validatable
 
-  has_many        :spots
+  has_many        :spots,        dependent: :destroy
   has_many        :comments,     dependent: :destroy
   has_many        :likes,        dependent: :destroy
   has_many        :liked_spots, through: :likes, source: :spot
